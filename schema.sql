9e6ec823469a128a7abfbbc2346839317aaeb17a
dbreznay <60904436+dbreznay@users.noreply.github.com>
2020-08-02 21:14:24 -0500

    ran npm json and created db folder

---
+++ schema.sql
@@ -0,0 +1,64 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema Employee_Tracker
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `Employee_Tracker` ;
+
+-- -----------------------------------------------------
+-- Schema Employee_Tracker
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Employee_Tracker` ;
+USE `Employee_Tracker` ;
+
+-- -----------------------------------------------------
+-- Table `Employee_Tracker`.`department`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Employee_Tracker`.`department` ;
+
+CREATE TABLE IF NOT EXISTS `Employee_Tracker`.`department` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Employee_Tracker`.`role`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Employee_Tracker`.`role` ;
+
+CREATE TABLE IF NOT EXISTS `Employee_Tracker`.`role` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(30) NULL,
+  `salary` VARCHAR(45) NULL,
+  `department_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Employee_Tracker`.`employee`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Employee_Tracker`.`employee` ;
+
+CREATE TABLE IF NOT EXISTS `Employee_Tracker`.`employee` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(30) NULL,
+  `last_name` VARCHAR(30) NULL,
+  `role_id` VARCHAR(30) NULL,
+  `manager_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
